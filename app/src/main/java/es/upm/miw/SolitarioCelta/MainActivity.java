79d307f54132818ca1c9fcbc55a517ff1cd53c8a
laucv <laura.canelada.velasco@alumnos.upm.es>
2019-10-20 22:05:50 +0200

    Implementado (bien) reiniciar partida #1

--- app/src/main/java/es/upm/miw/SolitarioCelta/MainActivity.java
+++ app/src/main/java/es/upm/miw/SolitarioCelta/MainActivity.java
@@ -99,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void reiniciarJuego(){
         miJuego.reiniciar();
+        mostrarTablero();
     }
 
     public void recuperarPartida(){
